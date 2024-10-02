3ac3378b3153d784f40e480e1ed517b65b3133ec
Maria Suzane <suzaane97@gmail.com>
2024-10-02 15:22:05 -0300

    alteração do comando filter para reduce e alteração de nomes de variaveis

--- exercicio-01/app.ts
+++ exercicio-01/app.ts
@@ -1,33 +1,32 @@
-function contaVogais( input?: string): number {
-    const vogais: string[] = ['a', 'e', 'i', 'o', 'u'] 
+function countVowels( input?: string): number {
+    const vowels: string[] = ['a', 'e', 'i', 'o', 'u'] 
 
-    let forminput : boolean = false
+    let formInput : boolean = false
     
     if (!input) {
-        const element = document.getElementById('palavra') as HTMLInputElement
+        const element = document.getElementById('word') as HTMLInputElement
         input = element.value
-        forminput = true
+        formInput = true
     }
 
-    let palavra: string = input.toLowerCase();
-    let resultado : number = palavra.split('').filter(letra => vogais.includes(letra)).length;
-    
-    if (forminput) {
-        const element2 = document.getElementById('resultado') as HTMLElement
-        element2.textContent = `A quantidade de vogais é ${resultado}`
+    let word: string = input.toLowerCase();
+    let result: number = word.split('').reduce((acc, letter) => { return vowels.includes(letter) ? acc + 1 : acc }, 0);    
+    if (formInput) {
+        const element2 = document.getElementById('result') as HTMLElement
+        element2.textContent = `A quantidade de vogais é ${result}`
     }
 
-    return resultado
+    return result
 
 }
 
 
 // previne o formulário de atualizar a página
-const formElement = document.getElementById('formulario') as HTMLFormElement
+const formElement = document.getElementById('form') as HTMLFormElement
 
 formElement.addEventListener('submit', function(event: Event) {
     event.preventDefault();
 })
 
 
-console.log(`A quantidade de vogais é ${contaVogais('Suzane')}`)
+console.log(`A quantidade de vogais é ${countVowels('Suzane')}`)
